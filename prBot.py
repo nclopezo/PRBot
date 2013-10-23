5db829e7282963b4504971bbefb572a02d29b043
David Mendez <david.mendez@cern.ch>
2013-10-23 11:45:39 +0200

    add lines after error in unit test is found. first version of parse matrix log

--- prBot.py
+++ prBot.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
 from optparse import OptionParser
 from github import Github
+import re
 
 #-----------------------------------------------------------------------------------
 #---- Parser Options
 #-----------------------------------------------------------------------------------
-parser = OptionParser(usage="usage: %prog ACTION [options] \n ACTION = TESTS_OK_PR | PARSE_UNIT_TESTS_FAIL | PARSE_BUILD_FAIL")
+parser = OptionParser(usage="usage: %prog ACTION [options] \n ACTION = TESTS_OK_PR | PARSE_UNIT_TESTS_FAIL | PARSE_BUILD_FAIL | PARSE_MATRIX_FAIL")
 
 parser.add_option("-u", action="store", type="string", dest="username", help="Your github account username", default='None')
 parser.add_option("-p", action="store", type="string", dest="password", help="Your github account password", default='None')
@@ -21,26 +22,53 @@ parser.add_option("--unit-tests-file", action="store", type="string", dest="unit
 #TODO
 
 #-------------------------------------------------------------------------------------
+ 
+def read_matrix_log_file(matrix_log):
+	errors_worfklows = {}
+	for line in open(matrix_log):
+		if 'ERROR executing' in line:
+			parts = line.split(" ")
+			print ''
+			print line
+			print "--------------------"
+			print parts
+			workflow = parts[4]
+			workflow = re.sub('_.*$', '', workflow)
+			print workflow
+
 def read_build_log_file(repo,build_log,tests_url):
 	pull_request = repo.get_pull(pr_number)
 	error_found = False
 	line_number = 0
 	error_line = 0
-	lines_to_keep=3
-	lines = ['']
+	lines_to_keep_before=5
+	lines_to_keep_after=5
+	lines_since_error=0
+	lines_before = ['']
+	lines_after = ['']
+	error_found = False
 	for line in open(build_log):
 		line_number += 1
-		lines.append(line)
-		if (line_number > lines_to_keep):
-			lines.pop(0)
+		if (not error_found):
+			lines_before.append(line)
+			if (line_number > lines_to_keep_before):
+				lines_before.pop(0)
 		if 'error: ' in line:
 			error_found = True
 			error_line = line_number
-			#print line
 		if error_found:
-			break
+			if (lines_since_error == 0):
+				lines_since_error += 1
+				continue
+			elif (lines_since_error <= lines_to_keep_after):
+				lines_since_error += 1
+				lines_after.append(line)
+			else:
+				break
 	message = '-1 \n I found an error when building: \n \n <pre>'
-	for line in lines:
+	for line in lines_before:
+		message += line + '\f'
+	for line in lines_after:
 		message += line + '\f'
 	message += '</pre> \n you can see the results of the tests here: \n %s ' % tests_url
 	print message 
@@ -136,5 +164,8 @@ elif (action == 'PARSE_BUILD_FAIL'):
 	tests_results_url='https://cmssdt.cern.ch/SDT/jenkins-artifacts/pull-request-integration/%d/summary.html' % pr_job_id
 	build_log_file = options.unit_tests_file
 	read_build_log_file(official_cmssw,build_log_file, tests_results_url)
+elif (action == 'PARSE_MATRIX_FAIL'):
+	matrix_log_file = options.unit_tests_file
+	read_matrix_log_file(matrix_log_file)
 else:
 	print "I don't recognize that action!"
